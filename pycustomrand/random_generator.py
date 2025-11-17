1147d4d60c2b45951f7528b5ac7b623b5a2e9551
n1xs1 <vanyalebedev45@gmail.com>
2025-11-17 15:48:55 +0300

    Added random_generator

---
+++ pycustomrand/random_generator.py
@@ -0,0 +1,37 @@
+from .custom_round import normal_round
+import time
+
+
+class PseudoRandom:
+	@staticmethod
+	def get_random_number(length=1):
+		number = ''
+		while len(number) != length:
+            # magic starts here
+			number += (str(int(((int(str(int((time.time_ns()*1.71)/0.8))[::-1])**0.5)*7)/9))[-1])
+			number = str(int(number))
+			time.sleep(0.001)
+		return int(number)
+
+	@staticmethod
+	def random():
+		return float("0."+str(PseudoRandom.get_random_number(16)))
+	
+	@staticmethod
+	def random_from_range(start, end):
+		return normal_round((end-start)*(PseudoRandom.random())+start)
+	
+	@staticmethod
+	def random_from_float_range(start, end):
+		return ((end-start)*(PseudoRandom.random())+start)
+	
+	@staticmethod
+	def choice(array):
+		return array[PseudoRandom.random_from_range(0, len(array)-1)]
+	
+	@staticmethod
+	def shuffle(array):
+		for _ in range(len(array)*2):
+			x1, x2 = PseudoRandom.random_from_range(0, len(array)-1), PseudoRandom.random_from_range(0, len(array)-1)
+			array[x1], array[x2] = array[x2], array[x1]
+		return array
