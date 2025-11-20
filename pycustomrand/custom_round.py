ebc0c0be64b9541f32b0d2cedbe61f5848b8a233
n1xs1 <vanyalebedev45@gmail.com>
2025-11-20 23:42:14 +0300

    Minor improvements

--- pycustomrand/custom_round.py
+++ pycustomrand/custom_round.py
@@ -1,14 +1,12 @@
-def normal_round(number, length=0):
-	try:
-		result = [int(i) for i in (str(number).split('.')[1])][::-1]
-		for i in range(0, len(result)-length-1):
-			if result[i] >= 5:
-				result[i+1] += 1
-				result[i] = 0
-		sec_number = [str(number).split('.')[0], ("".join([str(i) for i in result[::-1]]))]
-		if length == 0:
-			if int(sec_number[1][0]) >= 5:
-				return (int(sec_number[0])+1)
-			else: return int(sec_number[0])
-		else: return float(sec_number[0]+'.'+sec_number[1][0:length])
-	except: return number
+def normal_round(number, length=0) -> float:
+	digits = [int(i) for i in (str(number).split('.')[1])][::-1]
+	for i in range(0, len(digits)-length-1):
+		if digits[i] >= 5:
+			digits[i+1] += 1
+			digits[i] = 0
+	sec_number = [str(number).split('.')[0], ("".join([str(i) for i in digits[::-1]]))]
+	if length == 0:
+		if int(sec_number[1][0]) >= 5:
+			return (int(sec_number[0])+1)
+		else: return int(sec_number[0])
+	else: return float(sec_number[0]+'.'+sec_number[1][0:length])
