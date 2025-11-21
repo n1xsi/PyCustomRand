4bfee6991cddd49642683238468b76a38617807f
n1xs1 <vanyalebedev45@gmail.com>
2025-11-21 04:41:33 +0300

    Custom round full refactor with comments

--- pycustomrand/custom_round.py
+++ pycustomrand/custom_round.py
@@ -1,12 +1,17 @@
-def normal_round(number, length=0) -> float:
-	digits = [int(i) for i in (str(number).split('.')[1])][::-1]
-	for i in range(0, len(digits)-length-1):
+def true_round(number: float, length=0) -> float:
+	# Получение цифр после запятой заданного числа
+	digits = list(map(int, str(number).split('.')[1]))
+    
+	# Проход по цифрам в обратном порядке, начиная с той, которая находится на позиции length + 1
+	for i in range(len(digits)-length, -1, -1):
 		if digits[i] >= 5:
-			digits[i+1] += 1
+			digits[i-1] += 1
 			digits[i] = 0
-	sec_number = [str(number).split('.')[0], ("".join([str(i) for i in digits[::-1]]))]
+    
+    # Если кол-во знаков после запятой (length) не было задано
 	if length == 0:
-		if int(sec_number[1][0]) >= 5:
-			return (int(sec_number[0])+1)
-		else: return int(sec_number[0])
-	else: return float(sec_number[0]+'.'+sec_number[1][0:length])
+		# Тогда округляем до целого числа (+1 к числу, если первая цифра после запятой >= 5)
+		return float(str(number)[0]) + (1 if digits[0] >= 5 else 0)
+	else:
+		# Иначе формируем и возвращаем число с заданным кол-вом знаков после запятой
+		return float(str(number)[0] + '.' + ''.join(map(str, digits[:length])))
