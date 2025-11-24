22f71d9105b444dc5497119f16e0103f965dfac6
n1xs1 <vanyalebedev45@gmail.com>
2025-11-24 13:49:09 +0300

    Added type annotation and improved docs for class methods

--- pycustomrand/random_generator.py
+++ pycustomrand/random_generator.py
@@ -4,7 +4,8 @@ from time import sleep, time_ns
 
 class PseudoRandom:
 	@staticmethod
-	def get_random_number(length=1):
+	def get_random_number(length=1) -> int:
+		""" Генерация псевдослучайного числа заданной длины """
 		number = ''
 		while len(number) != length:
             # Генерация псевдослучайной цифры на основе текущего времени и "волшебных" математических операций
@@ -16,23 +17,32 @@ class PseudoRandom:
 		return int(number)
 
 	@staticmethod
-	def random():
+	def random() -> float:
+		""" Возвращает случайное число с плавающей точкой в диапазоне [0.0, 1.0) """
 		return float("0."+str(PseudoRandom.get_random_number(16)))
 	
 	@staticmethod
-	def random_from_range(start, end):
-		return true_round((end-start)*(PseudoRandom.random())+start)
+	def random_from_range(start: int, end: int) -> int:
+		""" Возвращает случайно выбранный элемент из диапазона [start, end) """ 
+		return int(true_round((end-start)*(PseudoRandom.random())+start))
 	
 	@staticmethod
-	def random_from_float_range(start, end):
+	def random_from_float_range(start, end) -> float:
+		"""
+  		Возвращает случайно выбранное число с плавающей точкой из диапазона [start, end)
+  		
+		start, end - могут быть как целыми числами, так и числами с плавающей точкой
+    	"""
 		return ((end-start)*(PseudoRandom.random())+start)
 	
 	@staticmethod
 	def choice(array):
+		""" Возвращает случайно выбранный элемент из непустого массива """
 		return array[int(PseudoRandom.random_from_range(0, len(array)))]
 	
 	@staticmethod
 	def shuffle(array):
+		""" Перемешивает массив на месте """
 		for _ in range(len(array)*2):
 			x1, x2 = int(PseudoRandom.random_from_range(0, len(array)-1)), int(PseudoRandom.random_from_range(0, len(array)-1))
 			array[x1], array[x2] = array[x2], array[x1]
