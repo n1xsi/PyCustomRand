5e79ce3c64d4c395782efdf339db6e8b324dc56b
n1xs1 <vanyalebedev45@gmail.com>
2025-12-27 16:32:22 +0300

    feat: sign and abs fixes + roundings comments add

--- pycustomrand/custom_round.py
+++ pycustomrand/custom_round.py
@@ -8,9 +8,12 @@ def true_round(number: float | int, length: int = 0) -> float | int:
     # Определение знака числа
     sign = -1 if number_parts[0][0] == '-' else 1
     # Создаём список цифр из числа (digits[0] - целая часть со знаком, digits[1] - дробная)
-    digits = [int(number_parts[0])*sign] + list(map(int, number_parts[1]))
+    digits = [abs(int(number_parts[0]))] + list(map(int, number_parts[1]))
 
     # Проход по цифрам в обратном порядке, начиная с той, что после нужного знака округления
+    ## "-1", т.к. первое значение в digits - это целая часть числа
+    ## (1 if length == 0 else 0) - корректная обработка округления до целого числа - проходимся по всем цифрам
+    # до первой цифры после запятой включительно
     for i in range(len(digits)-1-(1 if length == 0 else 0), length-1-(1 if length == 0 else -1), -1):
         # Если цифра на текущей позиции >= 5, то +1 к предыдущей цифре (правило округления)
         if digits[i] >= 5:
@@ -25,7 +28,7 @@ def true_round(number: float | int, length: int = 0) -> float | int:
     if length == 0:
         # Тогда округляем до целого числа (+1 к числу, если первая цифра после запятой >= 5)
         # используем split('.')[0], чтобы сохранить знак перед числом (+/-)
-        return float(digits[0]) + (1 if digits[1] >= 5 else 0)
+        return (float(digits[0]) + (1 if digits[1] >= 5 else 0))*sign
     else:
         # Иначе формируем и возвращаем число с заданным кол-вом знаков после запятой
-        return float(f"{digits[0]}.{''.join(map(str, digits[1:length+1]))}")
+        return float(f"{digits[0]}.{''.join(map(str, digits[1:length+1]))}")*sign
